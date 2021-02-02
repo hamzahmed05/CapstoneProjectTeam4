1c6d049edfc3e2db1f0a5ba74d102d99eb37f683
Roshan Jamkatel <roshan.jamkatel@gmail.com>
2021-02-01 18:53:49 -0600

    dash changed to project

--- team4/src/layout/SignedInLinks.js
+++ team4/src/layout/SignedInLinks.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux'
 const SignedInLinks = (props) => {
     return(
         <ul className="right">
-            <li><NavLink to='/dash'>Project</NavLink></li>
+            <li><NavLink to='/project'>Project</NavLink></li>
             <li><NavLink to='/' onClick={/*props.*/signout}>Log Out</NavLink></li>
             <li><NavLink to='/chat'>Chat</NavLink></li>
             <li><NavLink to='/' className='btn btn-floating blue lighten-4'>TP</NavLink></li>
