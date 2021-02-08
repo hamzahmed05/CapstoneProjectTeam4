8fddb449eca9ffd34d2201566b02917bd83014de
Sam Borcia <samuelborcia@gmail.com>
2021-02-07 21:24:08 -0600

    Created grid layout for home landing page

---
+++ team4/src/pages/Home.js
@@ -0,0 +1,24 @@
+<<<<<<< Updated upstream
+=======
+import React, {Component} from 'react';
+import './Home-Style.css';
+
+class Home extends Component{
+    render() {
+      return (
+          <div id="homepage-grid">
+     
+			<div class="wrapper">
+			  <div class="box sidebar">Profile</div>
+			  <div class="box sidebar2">Chat</div>
+			  <div class="box content">Project Overview
+				<br /> This is where the middle content goes.</div>
+			  <div class="box footer">Footer</div>
+			</div>
+		 </div>
+      )
+    }
+  }
+  
+  export default Home
+>>>>>>> Stashed changes
