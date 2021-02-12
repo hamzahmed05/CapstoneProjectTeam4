2a636331900d828f3ba155dc787940b5411006c2
Roshan Jamkatel <roshan.jamkatel@gmail.com>
2021-02-12 17:39:12 -0600

    Added Components into Landing page
    v 1.0

--- team4/src/pages/Home.js
+++ team4/src/pages/Home.js
@@ -1,29 +1,56 @@
 import React, {Component} from 'react';
 import './Home-Style.css';
 import {useHistory} from 'react-router-dom'
+import Chat from '../chat/chat'
+import ProjectModule from '../projects/projects'
+import { render } from 'react-dom';
+import Profile from './Profile';
+
 
 export default function Home(props){
+	
+
 	const history = useHistory();
 	function isNewUser(){
-		if(props.currentUser.accountType === "new"){
+		if(props.currentUser.accountType == "new"){
 		  history.push({
 			pathname: '/usersettings',
 			state: props.currentUser
 		  });
 		}
 	  }
+
+	  function isAlreadyAUser() {
+		  if (props.currentUser.accountType == "student"){
+				console.log(props.currentUser)
+				return(
+					<>
+						<img src={props.currentUser.photoURL}/>
+						<p> {props.currentUser.displayName}</p>
+					</>
+
+					)
+		  }
+	  }
+
       return (
           <div id="homepage-grid">
 			<div class="wrapper">
-			  <div class="box sidebar">Profile</div>
-			  <div class="box sidebar2">Chat</div>
+			  <div class="box sidebar">
+				  {isAlreadyAUser()}
+				  </div>
+			  <div class="box sidebar2"><Chat/></div>
 			  <div class="box content">Project Overview
-				<br /> This is where the middle content goes.</div>
-			  <div class="box footer">Footer</div>
+				<br /> 
+				<ProjectModule/></div>
+			  <div class="box footer">ECampus ©</div>
 			</div>
 			{isNewUser()}
 		 </div>
       )
   }
- 
+
+
+
+
 
