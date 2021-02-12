4f9a5cb1ddded3410510611997da0b07484166f1
Roshan Jamkatel <roshan.jamkatel@gmail.com>
2021-02-12 16:17:17 -0600

    test

--- team4/src/pages/Home.js
+++ team4/src/pages/Home.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import './Home-Style.css';
-import {useHistory} from 'react-router-dom';
-import Profile from './Profile';
-import Chat from '../chat/chat';
+import {useHistory} from 'react-router-dom'
 
 export default function Home(props){
 	const history = useHistory();
@@ -18,7 +16,7 @@ export default function Home(props){
           <div id="homepage-grid">
 			<div class="wrapper">
 			  <div class="box sidebar">Profile</div>
-			  <div class="box sidebar2"><Chat/></div>
+			  <div class="box sidebar2">Chat</div>
 			  <div class="box content">Project Overview
 				<br /> This is where the middle content goes.</div>
 			  <div class="box footer">Footer</div>
