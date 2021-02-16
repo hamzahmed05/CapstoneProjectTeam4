9491aa3844a9d0425dfebddd8b9f111ef0f27c89
Roshan Jamkatel <roshan.jamkatel@gmail.com>
2021-02-15 18:12:58 -0600

    Added project clicking + scrolling for chat
    Added items from my branch to main, since css conflicts decided to just recode the small changes.

--- team4/src/projects/projectInformation.js
+++ team4/src/projects/projectInformation.js
@@ -4,11 +4,11 @@ import { db, firestore } from '../services/firebase'
 
 const ProjectInformation = () => {
     
-    
+    /* currently the href is only mapped to project - 1 (dummy data purpose); need to use redux on this */
     return(
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
+                <span className="card-title"><a href="/project/:1">Project Title</a></span> 
                 <p> Posted by Team 4</p>
                 <p className="grey-text">1/25/2021</p>
             </div>
